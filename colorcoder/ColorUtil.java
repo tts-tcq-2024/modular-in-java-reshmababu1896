50b2c00a5f33bc88ea09800631b8eabdf820a4a8
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-19 15:52:39 +0530

    Update ColorUtil.java

--- colorcoder/ColorUtil.java
+++ colorcoder/ColorUtil.java
@@ -1,6 +1,8 @@
 package colorcoder;
 
 public class ColorUtil {
+    private final int index;
+    private final String name;
     public static ColorPair getColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor.Color majorColor = MajorColor.Color.fromIndex(zeroBasedPairNumber / MinorColor.COUNT);
@@ -11,5 +13,27 @@ public class ColorUtil {
     public static int getPairNumberFromColor(MajorColor.Color major, MinorColor.Color minor) {
         return major.getIndex() * MinorColor.COUNT + minor.getIndex() + 1;
     }
-    
+
+    protected Color(int index, String name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static <E extends Color> E fromIndex(E[] values, int index) {
+        for (E color : values) {
+            if (color.getIndex() == index) {
+                return color;
+            }
+        }
+        return null;
+    }
 }
