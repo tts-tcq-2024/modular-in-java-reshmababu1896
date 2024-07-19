076acb280978a60b77ed11ad495283d6cd35dd7a
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-19 16:02:00 +0530

    Update MinorColor.java

--- colorcoder/MinorColor.java
+++ colorcoder/MinorColor.java
@@ -17,16 +17,6 @@ public class MinorColor {
         int getIndex() {
             return index;
         }
-
-    //     public static Color fromIndex(int index) {
-    //         for (Color color : Color.values()) {
-    //             if (color.getIndex() == index) {
-    //                 return color;
-    //             }
-    //         }
-    //         return null;
-    //     }
-    // }
      public static MinorColor fromIndex(int index) {
         return ColorUtil.fromIndex(values, index);
     }
