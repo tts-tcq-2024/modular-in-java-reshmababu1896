bd8e9f376e74bc02b4204abd727181604a72e7b6
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-20 19:03:50 +0530

    Update ColorUtil.java

--- colorcoder/ColorUtil.java
+++ colorcoder/ColorUtil.java
@@ -1,8 +1,6 @@
 package colorcoder;
 
 public class ColorUtil {
-    private final int index;
-    private final String name;
     public static ColorPair getColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor.Color majorColor = MajorColor.Color.fromIndex(zeroBasedPairNumber / MinorColor.COUNT);
@@ -13,43 +11,4 @@ public class ColorUtil {
     public static int getPairNumberFromColor(MajorColor.Color major, MinorColor.Color minor) {
         return major.getIndex() * MinorColor.COUNT + minor.getIndex() + 1;
     }
-
-    protected ColorUtil(int index, String name) {
-        this.index = index;
-        this.name = name;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    // @Override
-    // public String toString() {
-    //     return name;
-    // }
-
-    public static <E extends ColorUtil> E fromIndex(E[] values, int index) {
-        for (E color : values) {
-            if (color.getIndex() == index) {
-                return color;
-            }
-        }
-        return null;
-    }
-
-    //  public static String generateReferenceManual() {
-    //     StringBuilder manual = new StringBuilder();
-    //     manual.append("Color Code Reference Manual\n");
-    //     manual.append("Pair Number | Major Color | Minor Color\n");
-    //     manual.append("---------------------------------------\n");
-
-    //     int pairNumber = 1;
-    //     for (MajorColor major : MajorColor.values()) {
-    //         for (MinorColor minor : MinorColor.values()) {
-    //             manual.append(String.format("%-11d| %-11s| %-11s\n", pairNumber, major, minor));
-    //             pairNumber++;
-    //         }
-    //     }
-    //     return manual.toString();
-    // }
 }
