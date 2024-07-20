cbe5bcbab17ff5956f76cceefeba28481ee9b51a
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-20 19:06:12 +0530

    Update MinorColor.java

--- colorcoder/MinorColor.java
+++ colorcoder/MinorColor.java
@@ -1,12 +1,12 @@
 package colorcoder;
 
-public class MajorColor {
+public class MinorColor {
     public enum Color {
-        WHITE(0),
-        RED(1),
-        BLACK(2),
-        YELLOW(3),
-        VIOLET(4);
+        BLUE(0),
+        ORANGE(1),
+        GREEN(2),
+        BROWN(3),
+        SLATE(4);
 
         private int index;
 
@@ -29,7 +29,7 @@ public class MajorColor {
     }
 
     public static final String[] NAMES = {
-            "White", "Red", "Black", "Yellow", "Violet"
+            "Blue", "Orange", "Green", "Brown", "Slate"
     };
 
     public static final int COUNT = NAMES.length;
