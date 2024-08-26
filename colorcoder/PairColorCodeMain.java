cb5acede58bc578dacb18824e537fe7aaa0e88d8
reshmababu1896 <reshmababu1896@gmail.com>
2024-08-26 11:04:27 +0530

    Update PairColorCodeMain.java

--- colorcoder/PairColorCodeMain.java
+++ colorcoder/PairColorCodeMain.java
@@ -3,41 +3,25 @@ package colorcoder;
 public class PairColorCodeMain {
     public static ColorPair numberToPair(int pairNumber, MajorColor.Color expectedMajor, MinorColor.Color expectedMinor) {
         ColorPair colorPair = ColorUtil.getColorFromPairNumber(pairNumber);
-       return colorPair;
-        // assert(colorPair.getMajor() == expectedMajor);
-        // assert(colorPair.getMinor() == expectedMinor);
+           return colorPair;
     }
 
     public static int pairToNumber(MajorColor.Color major, MinorColor.Color minor, int expectedPairNumber) {
         int pairNumber = ColorUtil.getPairNumberFromColor(major, minor);
-     return pairNumber;
-        // assert(pairNumber == expectedPairNumber);
+         return pairNumber;
     }
     public static void generateColorCodeManual(){
         MajorColor.Color[] majorColors = MajorColor.Color.values();
         MinorColor.Color[] minorColors = MinorColor.Color.values();
-
         String referenceManual = ColorUtil.generateReferenceManual(majorColors, minorColors);
         printManual(referenceManual);
     }
     private static void printManual(String referenceManual){
        System.out.println(referenceManual);  
     }
-    
-    public static void printMessage(String message){
-        System.out.println(message);
-    }
 
     public static void main(String[] args) {
         generateColorCodeManual();
-        // testNumberToPair(4, MajorColor.Color.WHITE, MinorColor.Color.BROWN);
-        // testNumberToPair(5, MajorColor.Color.WHITE, MinorColor.Color.SLATE);
-
-        // testPairToNumber(MajorColor.Color.BLACK, MinorColor.Color.ORANGE, 12);
-        // testPairToNumber(MajorColor.Color.VIOLET, MinorColor.Color.SLATE, 25);
-
-
-        
     }
     
 }
